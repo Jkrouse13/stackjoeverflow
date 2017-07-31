85c4dbc0cd134beb68a45c5fb52a872facb9182f
jkrouse13 <jon.krouse@gmail.com>
2017-07-31 10:42:38 -0400

    some of these files might be in the wrong place.

---
+++ app/controllers/queries_controller.rb
@@ -0,0 +1,28 @@
+class QueriesController < ApplicationController
+  def index
+    @queries = Query.all
+  end
+
+  def show
+    @query = Query.find(params[:id])
+  end
+
+  def new
+    @query = Query.new
+  end
+
+  def create
+    @query = Query.create(query_params)
+
+    if @query.save
+      redirect_to @query
+    else
+      render :new
+    end
+  end
+
+  private
+  def query_params
+    params.require(:query).permit(:title, :body)
+  end
+end
